dd674840ff6489a4b2f1813fa4e877a60fbf0609
Irving Alain Aguilar Pérez <m4uz@MacBook-Pro-de-Irving.local>
2020-04-17 15:44:03 -0500

    Update: exec

--- act_colab04/execute.c
+++ act_colab04/execute.c
@@ -25,18 +25,9 @@
 #include <math.h>
 #include <setjmp.h>
 #include <signal.h>
-jmp_buf env1, env2, env3;
 typedef unsigned long ulong;
-void handler(int sig) {
-	if (sig == SIGUSR1) {
-    longjmp(env1, 1);
-  } else if (sig == SIGUSR2) {
-    longjmp(env2, 1);
-  } else if (sig == SIGHUP) {
-    longjmp(env3, 1);
-  }
-}
-
+char *l1, *l2, *l3, *l4, *l5, *l6;
+char cmd1[100], cmd2[100], cmd3[100];
 // void readFile(){
 //     FILE *file = fopen("file1.txt", "r");
 //     if(file){
@@ -88,46 +79,120 @@ void handler(int sig) {
 //     }
 
 //}
-int main(int argc, char* argv[]) {
-    // int file1, file2, file3;
-    // signal(SIGUSR1, handler);
-    // signal(SIGUSR2, handler);
-    // signal(SIGHUP, handler);
 
-    // if (argc != 4) {
-    //     fprintf(stderr, "usage: %s file1 file2 file3\n", argv[0]);
-    //     return -2;
-    // }
-    // /* Check if the file exists */
-  	// if((file1 = open(argv[1], O_RDONLY)) < 0)
-    // {
-    //     fprintf(stderr, "%s: the file %s does not exist\n", argv[0], argv[1]);
-    //   	return -3;
-    // }
-    // if ((file2 = open(argv[2], O_RDONLY)) < 0) {
-    //     fprintf(stderr, "%s: the file %s does not exist\n", argv[0],argv[2]);
-	//     return -4;
-    // }
-    // if ((file3 = open(argv[3], O_RDONLY)) < 0) {
-    //     fprintf(stderr, "%s: the file %s does not exist\n", argv[0],argv[3]);
-    //     return -5;
-    // }
-    // ulong f_size;
-    // f_size = lseek(file1, 0, SEEK_END);
-    // if (f_size < 1) {
-    //     fprintf(stderr, "%s: the file %s is empty\n", argv[0],argv[1]);
-    //     return -6; 
-    // }
-    // f_size = lseek(file2, 0, SEEK_END);
-    // if (f_size < 1) {
-    //     fprintf(stderr, "%s: the file %s is empty\n", argv[0],argv[2]);
-    //     return -7; 
-    // }
-    // f_size = lseek(file3, 0, SEEK_END);
-    // if (f_size < 1) {
-    //     fprintf(stderr, "%s: the file %s is empty\n", argv[0],argv[3]);
-    //     return -8; 
-    // }
-    quitarcomas();
+/* Read commands from file */
+int readFromFile(FILE* file, char* buffer){
+    fseek(file, 0L, SEEK_END);
+    long ptr = ftell(file);
+    rewind(file);
+    if (buffer != NULL)
+    {
+        fread(buffer, ptr, 1, file);
+        fclose(file);
+        return 1;
+    }
+}/* Separate commas */
+void separateCommas(char* cmdopt){
+    l1 = strtok(cmdopt, ",");
+    l2 = strtok(NULL, ",");
+}
+/* Signal Handler */
+void processSignal(int signalnum){
+    int pid;
+    switch (signalnum)
+    {
+    case SIGUSR1:
+            if ((pid = fork()) < 0)
+            {
+                perror("No child process");
+            }
+            else if (pid == 0)
+            {
+                execlp(l1, l2, NULL);
+            }
+            sleep(3);
+        break;
+        case SIGUSR2:
+            if ((pid = fork()) < 0)
+            {
+                perror("No child process");
+            }
+            else if (pid == 0)
+            {
+                execlp(l1, l2, NULL);
+            }
+            sleep(3);
+            break;
+        case SIGPWR:
+            if ((pid = fork()) < 0)
+            {
+                perror("No child process");
+            }
+            else if (pid == 0)
+            {
+                execlp(l1, l2, NULL);
+            }
+            sleep(3);
+            break;
+        case SIGINT:
+            printf("Ending...");
+            _exit(0);
+            break;         
+    }
+}
+int main(int argc, char* argv[]) {
+    int file1, file2, file3;
+    char cmd[100];
+    if (argc != 4) {
+        fprintf(stderr, "usage: %s file1 file2 file3\n", argv[0]);
+        return -2;
+    }
+    /* Check if the file exists */
+  	if((file1 = open(argv[1], O_RDONLY)) < 0)
+    {
+        fprintf(stderr, "%s: the file %s does not exist\n", argv[0], argv[1]);
+      	return -3;
+    }
+    if ((file2 = open(argv[2], O_RDONLY)) < 0) {
+        fprintf(stderr, "%s: the file %s does not exist\n", argv[0],argv[2]);
+	    return -4;
+    }
+    if ((file3 = open(argv[3], O_RDONLY)) < 0) {
+        fprintf(stderr, "%s: the file %s does not exist\n", argv[0],argv[3]);
+        return -5;
+    }
+    ulong f_size;
+    f_size = lseek(file1, 0, SEEK_END);
+    if (f_size < 1) {
+        fprintf(stderr, "%s: the file %s is empty\n", argv[0],argv[1]);
+        return -6; 
+    }
+    f_size = lseek(file2, 0, SEEK_END);
+    if (f_size < 1) {
+        fprintf(stderr, "%s: the file %s is empty\n", argv[0],argv[2]);
+        return -7; 
+    }
+    f_size = lseek(file3, 0, SEEK_END);
+    if (f_size < 1) {
+        fprintf(stderr, "%s: the file %s is empty\n", argv[0],argv[3]);
+        return -8; 
+    }
+    /* Get the commands from files */
+    // FILE 1
+    readFromFile(file1, cmd1);
+    separateCommas(&cmd1);
+    // FILE 2
+    readFromFile(file2, cmd2);
+    separateCommas(&cmd2);
+    // FILE 3
+    readFromFile(file3, cmd3);
+    separateCommas(&cmd3);
+    // Signals
+    signal(SIGUSR1, processSignal);
+    signal(SIGUSR2, processSignal);
+    signal(SIGPWR, processSignal);
+    signal(SIGINT, processSignal);
+    printf("Waiting for signal...");
+    while(1);
   return 0;
 }
